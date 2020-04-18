6a60170a72b42c873a9100b2828e0dbb452b80e5
softMaina <maina.nganga15@students.dkut.ac.ke>
2020-04-18 20:46:55 +0300

    migrate to docker

---
+++ app/view/report.py
@@ -0,0 +1,22 @@
+def get_all_users():
+    pass
+
+
+def get_rejected_documents():
+    pass
+
+
+def get_accepted_documents():
+    pass
+
+
+def track_files():
+    pass
+
+
+def get_escalated_issues():
+    pass
+
+
+def open_file():
+    pass
--- edms/settings.py
+++ edms/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'edms.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-DOCKER = False
+DOCKER = True
 
 if DOCKER:
     DATABASES = {
