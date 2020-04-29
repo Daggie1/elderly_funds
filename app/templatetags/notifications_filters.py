b89a5ea874c427debb10cd1001275bf7055b633f
Douglas57 <douglasirungu57@gmail.com>
2020-04-29 19:09:19 +0300

    fixed notifications display error

---
+++ app/templatetags/notifications_filters.py
@@ -0,0 +1,21 @@
+from django import template
+from app.models import Notification
+import urllib
+
+register = template.Library()
+
+
+@register.filter
+def unread_notification_count(all_notifications=Notification.objects.none()):
+
+
+    print(all_notifications)
+    all_notifications.filter(read_at=None)
+    return all_notifications.count()
+@register.filter
+def unread_notification_list(all_notifications=Notification.objects.none()):
+
+
+    print(all_notifications)
+    all_notifications.filter(read_at=None)
+    return all_notifications
