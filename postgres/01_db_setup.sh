7caf5d02583a59aae2c898507313f2ec499d27d2
softMaina <maina.nganga15@students.dkut.ac.ke>
2020-05-01 17:02:57 +0300

    add default profile

--- postgres/01_db_setup.sh
+++
@@ -1,3 +0,0 @@
-psql -U postgres -c "CREATE USER postgres PASSWORD 'toor'"
-psql -U postgres -c "CREATE DATABASE edms7 OWNER postgres"
-psql -U postgres -c "GRANT ALL PRIVILEGES ON DATABASE edms7 TO postgres"
